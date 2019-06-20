80cc7a9062ae316e571ff0d7e9dc3f834c08b2c7
GBTC408001UR <GBTC408001UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-20 09:38:15 -0400

    Initial commit

---
+++ src/com/byaj/Main.java
@@ -0,0 +1,21 @@
+package com.byaj;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.print("First number -> ");
+        int x = read.nextInt();
+        int y = read.nextInt();
+
+        for (int i = x; i <= y; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " EVEN");
+            } else {
+                System.out.println(i + " ODD");
+            }
+        }
+    }
+}
